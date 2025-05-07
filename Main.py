a9c9d372176a9ee16d8bccd5dafd06d1e6d13306
Brunoluiz19 <brunolp3@al.insper.edu.br>
2025-05-07 16:38:20 -0300

    cria a tela do jogo

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
-# inicio dos arquivos e teste de commit em conjunto 
+import pygame
+from cores import branco
+
+pygame.init()
+
+# Medições da tela 
+largura = 800
+altura = 600
+
+# Inicia a tela 
+tela = pygame.display.set_mode((largura,altura))
+
+# Nome do jogo 
+pygame.display.set_caption("GALAGA")
+
+# Define o fundo  
+cor_fundo = (branco)
