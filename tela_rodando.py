ece14e9421120feb74b6232d4ab1ddf28cee5930
Brunoluiz19 <brunolp3@al.insper.edu.br>
2025-05-08 15:16:12 -0300

    rodando jogo 1.4

--- tela_rodando.py
+++ tela_rodando.py
@@ -26,3 +26,27 @@ pygame.display.set_caption("GALAGA")
 # Carregar e redimensionar imagem de fundo
 background_image = pygame.image.load("fundo_roxo.jpg")
 background_image = pygame.transform.scale(background_image, (largura, altura))
+
+FONT = pygame.font.SysFont('Orbitron', 20, bold=True)
+LARGE_FONT = pygame.font.SysFont('Orbitron', 32, bold=True)
+clock = pygame.time.Clock()
+FPS = 60
+
+# configuração jogador
+
+largura_jogador = 50
+altura_jogador = 38
+velocidade_jogador = 5
+
+# configuração do tiro
+largura_tiro = 7
+altura_tiro = 13
+velocidade_tiro_jogador = 7
+velocidade_tiro_inimigo = 3
+
+linha_inimigo = 4
+coluna_inimigo = 7
+inimigo_h_espaco = 50
+inimigo_v_espaco = 44
+inicio_inimigo_x = 25
+inicio_inimigo_y = 60
