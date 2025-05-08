d7b241d474181860ebd08e1addc8b33ced559d6d
erick <erickmoreno150306@gmail.com>
2025-05-08 14:35:51 -0300

    tela inicial 1.4

--- tela_inicial.py
+++ tela_inicial.py
@@ -1,48 +1,45 @@
+import os
 import pygame
 import sys
+import subprocess
 from cores import branco
+
+# Centraliza a janela
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 pygame.init()
 
 # Medições da tela 
-largura = 800
-altura = 600
+largura = 1000
+altura = 800
 
 # Inicia a tela 
 tela = pygame.display.set_mode((largura, altura))
-
-# Nome do jogo 
 pygame.display.set_caption("GALAGA")
 
-# Define o fundo  
+# Carrega e redimensiona o fundo
 fundos = pygame.image.load("titulo.png")  
-afundo = pygame.transform.scale(fundos, (largura, altura))
+fundo = pygame.transform.scale(fundos, (largura, altura))
 
 # Configuração do botão START
-fonte_subtitulo = pygame.font.Font(None, 45)  # Tamanho e fonte da letra  
-texto = fonte_subtitulo.render('START', True, (255,36,0))  # Mensagem e definição da cor 
-
-# Ajusta a posição do botão para que fique abaixo da nave
+fonte_subtitulo = pygame.font.Font(None, 45)  
+texto = fonte_subtitulo.render('START', True, (255, 36, 0))  
 titulo_pos = (largura // 2 - texto.get_width() // 2, altura - 150)  
+rect_start = pygame.Rect(titulo_pos[0], titulo_pos[1], texto.get_width(), texto.get_height())
 
 jogo = True
 
 while jogo:
 
-    # Verificação de eventos 
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             jogo = False
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            if rect_start.collidepoint(evento.pos):
+                pygame.quit()
+                subprocess.run([sys.executable, "tela_rodando.py"])
+                sys.exit()
 
-    # Desenha o fundo na tela
-    tela.blit(fundos, (0, 0))
-
-    # Desenha o botão START na tela abaixo da nave
+    tela.blit(fundo, (0, 0))
     tela.blit(texto, titulo_pos)
-
-    # Atualiza a tela 
     pygame.display.flip()
-
-# Encerra o Pygame 
-pygame.quit()
-sys.exit()
